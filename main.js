1816ac278f6716ac91afb5bbb4dedfe2722a6b97
Nathan Chu <63111210+nthnchu@users.noreply.github.com>
2020-08-23 15:19:47 -0400

    test

--- main.js
+++ main.js
@@ -10,10 +10,9 @@ const octokit = new Octokit({ auth: token })
 const payload = actionsGithub.context.payload
 const label = actionsCore.getInput('label')
 const issue_number = actionsCore.getInput('issue') ? actionsCore.getInput('issue') : payload.issue.number.toString()
-if (!issue_number) {
-  console.log(typeof issue_number)
-  actionsCore.setFailed("I cannot seem to find an issue number to comment on. If you think this is a bug, report an issue at https://github.com/issuekit/action/issues");
-}
+
+console.log(typeof comment)
+
 if (comment) {
   octokit.request(`POST /repos/${full_repo}/issues/${issue_number}/comments`, {
     owner: repo[0],
