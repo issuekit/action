a96420325ba8fd86de456c608ba5e92197a32910
Nathan Chu <63111210+nthnchu@users.noreply.github.com>
2020-08-23 15:40:23 -0400

    maybe this will work

--- main.js
+++ main.js
@@ -11,18 +11,22 @@ const payload = actionsGithub.context.payload
 const label = actionsCore.getInput('label').split(',')
 const issue_number = actionsCore.getInput('issue') ? actionsCore.getInput('issue') : payload.issue.number.toString()
 if (comment) {
-  octokit.request(`POST /repos/${full_repo}/issues/${issue_number}/comments`, {
-    owner: repo[0],
-    repo: repo[1],
-    issue_number: parseInt(issue_number),
-    body: comment
-  })
+  async () => {
+    octokit.request(`POST /repos/${full_repo}/issues/${issue_number}/comments`, {
+      owner: repo[0],
+      repo: repo[1],
+      issue_number: parseInt(issue_number),
+      body: comment
+    })
+  }
 }
 if (label) {
-  octokit.request(`POST /repos/${full_repo}/issues/${issue_number}/labels`, {
-    owner: repo[0],
-    repo: repo[1],
-    issue_number: parseInt(issue_number),
-    labels: label
-  })
+  async () => {
+    octokit.request(`POST /repos/${full_repo}/issues/${issue_number}/labels`, {
+      owner: repo[0],
+      repo: repo[1],
+      issue_number: parseInt(issue_number),
+      labels: label
+    })
+  }
 }
