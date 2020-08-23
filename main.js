b681e9debbe7df3e551ba4d32c7a28d39266a850
Nathan Chu <63111210+nthnchu@users.noreply.github.com>
2020-08-23 15:16:11 -0400

    testing the new labels

--- main.js
+++ main.js
@@ -8,10 +8,26 @@ const repo = full_repo.split('/')
 const comment = actionsCore.getInput('comment')
 const octokit = new Octokit({ auth: token })
 const payload = actionsGithub.context.payload
+const label = actionsCore.getInput('label')
 const issue_number = actionsCore.getInput('issue') ? actionsCore.getInput('issue') : payload.issue.number.toString()
-octokit.request(`POST /repos/${full_repo}/issues/${issue_number}/comments`, {
-  owner: repo[0],
-  repo: repo[2],
-  issue_number: parseInt(issue_number),
-  body: comment
-})
+if (!issue_number) {
+  console.log(typeof issue_number)
+  actionsCore.setFailed("I cannot seem to find an issue number to comment on. If you think this is a bug, report an issue at https://github.com/issuekit/action/issues");
+}
+if (comment) {
+  octokit.request(`POST /repos/${full_repo}/issues/${issue_number}/comments`, {
+    owner: repo[0],
+    repo: repo[2],
+    issue_number: parseInt(issue_number),
+    body: comment
+  })
+}
+if (label) {
+  console.log(typeof label)
+  // octokit.request(`POST /repos/${full_repo}/issues/${issue_number}/comments`, {
+  //   owner: repo[0],
+  //   repo: repo[2],
+  //   issue_number: parseInt(issue_number),
+  //   body: comment
+  // })
+}
