7d5d5cf98aeb75a319cc4c3b224f5895cf8344ae
Nathan Chu <63111210+nthnchu@users.noreply.github.com>
2020-08-23 16:21:22 -0400

    Update main.js

--- main.js
+++ main.js
@@ -22,10 +22,12 @@ if (comment) {
   // }
 }
 // if (label) {
-  octokit.request(`POST /repos/${full_repo}/issues/${issue_number}/labels`, {
+  octokit.request(`POST /repos/{owner}/{repo}/issues/{issue_number}/labels`, {
     owner: repo[0],
     repo: repo[1],
     issue_number: parseInt(issue_number),
-    labels: ['enhancement']
+    labels: [
+      'enhancement'
+    ]
   })
 // }
