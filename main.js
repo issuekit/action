8e4ed15f9fe6b0d3f206358f694cf93e0e9b7cbd
Nathan Chu <63111210+nthnchu@users.noreply.github.com>
2020-08-23 15:25:39 -0400

    Add label feature

--- main.js
+++ main.js
@@ -8,23 +8,21 @@ const repo = full_repo.split('/')
 const comment = actionsCore.getInput('comment')
 const octokit = new Octokit({ auth: token })
 const payload = actionsGithub.context.payload
-const label = actionsCore.getInput('label')
+const label = actionsCore.getInput('label').split(',')
 const issue_number = actionsCore.getInput('issue') ? actionsCore.getInput('issue') : payload.issue.number.toString()
-console.log(comment)
 if (comment) {
   octokit.request(`POST /repos/${full_repo}/issues/${issue_number}/comments`, {
     owner: repo[0],
-    repo: repo[2],
+    repo: repo[1],
     issue_number: parseInt(issue_number),
     body: comment
   })
 }
 if (label) {
-  console.log(typeof label)
-  // octokit.request(`POST /repos/${full_repo}/issues/${issue_number}/comments`, {
-  //   owner: repo[0],
-  //   repo: repo[2],
-  //   issue_number: parseInt(issue_number),
-  //   body: comment
-  // })
+  octokit.request(`POST /repos/${full_repo}/issues/${issue_number}/labels`, {
+    owner: repo[0],
+    repo: repo[1],
+    issue_number: parseInt(issue_number),
+    labels: label
+  })
 }
