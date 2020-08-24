86d044094723c1e0a17fa94282c192bf6b3bda18
Nathan Chu <63111210+nthnchu@users.noreply.github.com>
2020-08-24 18:13:39 -0400

    remove node_modules

--- main.js
+++ main.js
@@ -5,13 +5,17 @@ const { request } = require('@octokit/request')
 const token = core.getInput('token')
 const full_repo = process.env.GITHUB_REPOSITORY
 const repo = full_repo.split('/')
-const comment = core.getInput('comment')
 const payload = github.context.payload
+const comment = core.getInput('comment')
+const assign = core.getInput('assign')
 const label = core.getInput('label').split(',')
-const issue_number = parseInt(core.getInput('issue') ? core.getInput('issue') : payload.issue.number)
-console.log('7')
+
+const issue_number = parseInt(core.getInput('issueOverridesWebhook') ?
+  (core.getInput('issue') ? core.getInput('issue') : payload.issue.number) :
+  (payload.issue.number ? payload.issue.number : core.getInput('issue')))
+
 if (comment) {
-  request(`POST /repos/:owner/:repo/issues/:issue_number/comments`, {
+  request('POST /repos/:owner/:repo/issues/:issue_number/comments', {
     headers: {
       authorization: 'token ' + token,
     },
@@ -19,6 +23,17 @@ if (comment) {
     repo: repo[1],
     issue_number: issue_number,
     body: comment
+  })
+}
+if (assign) {
+  request('POST /repos/:owner/:repo/issues/:issue_number/assignees', {
+    headers: {
+      authorization: 'token ' + token,
+    },
+    owner: repo[0],
+    repo: repo[1],
+    issue_number: issue_number,
+    assignees: assign
   })
 }
 if (label) {
